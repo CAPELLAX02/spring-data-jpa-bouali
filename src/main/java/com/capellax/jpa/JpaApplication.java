4f5509bed3a07ef9714b3f061ca4b02d186c0853
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-03 22:42:30 +0300

    Insert some data into the database using command line runner

--- src/main/java/com/capellax/jpa/JpaApplication.java
+++ src/main/java/com/capellax/jpa/JpaApplication.java
@@ -1,13 +1,33 @@
 package com.capellax.jpa;
 
+import com.capellax.jpa.models.Author;
+import com.capellax.jpa.repositories.AuthorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthorRepository authorRepository
+	) {
+		return args -> {
+			var author = Author.builder()
+					.firstName("John")
+					.lastName("Doe")
+					.age(25)
+					.email("john@email.com")
+					.build();
+
+			authorRepository.save(author);
+		};
 	}
 
 }
--- src/main/java/com/capellax/jpa/models/Author.java
+++ src/main/java/com/capellax/jpa/models/Author.java
@@ -1,25 +1,25 @@
 package com.capellax.jpa.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
-@Table(name = "AUTHOR_TABLE")
 public class Author {
 
     @Id
     @GeneratedValue
     private Integer id;
 
-    @Column(
-            name = "f_name",
-            length = 35
-    )
     private String firstName;
 
     private String lastName;
@@ -31,16 +31,5 @@ public class Author {
     private String email;
 
     private Integer age;
-
-    @Column(
-            updatable = false,
-            nullable = false
-    )
-    private LocalDateTime createdAt;
-
-    @Column(
-            insertable = false
-    )
-    private LocalDateTime lastModifiedAt;
 
 }
