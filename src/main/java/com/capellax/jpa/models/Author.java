600edee827b4caa3f4b2a852a7d22de32c012161
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-03 21:39:09 +0300

    Table generation for the author entity.

--- src/main/java/com/capellax/jpa/models/Author.java
+++ src/main/java/com/capellax/jpa/models/Author.java
@@ -15,14 +15,23 @@ public class Author {
 
     @Id
     @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "author_sequence"
+            strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
     )
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id",
+            valueColumnName = "id_value",
+            allocationSize = 1
+    )
+    /*
     @SequenceGenerator(
             name = "author_sequence",
             sequenceName = "author_sequence",
             allocationSize = 1
     )
+    */
     private Integer id;
 
     private String firstName;
