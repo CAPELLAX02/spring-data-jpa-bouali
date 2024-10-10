fa93957db61c2bd9bf41892f69aa4f62e7d0bed7
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-10 15:52:20 +0300

    single table strategy on resource entity.

--- src/main/java/com/capellax/jpa/models/File.java
+++ src/main/java/com/capellax/jpa/models/File.java
@@ -1,15 +1,19 @@
 package com.capellax.jpa.models;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
-public class File {
+@Entity
+public class File extends Resource {
 
     private String type;
 
--- src/main/java/com/capellax/jpa/models/Resource.java
+++ src/main/java/com/capellax/jpa/models/Resource.java
@@ -2,15 +2,16 @@ package com.capellax.jpa.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Resource {
 
     @Id
