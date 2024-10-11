7ebd8af42b3229999a4ff21900030177296f005c
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-11 12:37:07 +0300

    Reserved word -order- fix. @Table annotataion used.

--- src/main/java/com/capellax/jpa/models/embedded/Order.java
+++ src/main/java/com/capellax/jpa/models/embedded/Order.java
@@ -2,6 +2,7 @@ package com.capellax.jpa.models.embedded;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "_order")
 public class Order {
 
     @EmbeddedId
