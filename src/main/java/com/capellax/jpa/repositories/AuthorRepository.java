13ce488d94d52ed08ddd9154c70093b0724f91ad
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-11 13:20:26 +0300

    playing with derived query methods that JPA Reository provides.

--- src/main/java/com/capellax/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/capellax/jpa/repositories/AuthorRepository.java
@@ -3,9 +3,27 @@ package com.capellax.jpa.repositories;
 import com.capellax.jpa.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository
         extends JpaRepository<Author, Integer> {
 
+    // select * from author where first_name = 'Ali'
+    List<Author> findAllByFirstName(String firstName);
 
+    // select * from author where first_name = 'ali'
+    List<Author> findAllByFirstNameIgnoreCase(String firstName);
+
+    // select * from author where first_name is like '%al%'
+    List<Author> findAllByFirstNameContainingIgnoreCase(String firstName);
+
+    // select * from author where first_name is like 'al%'
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String lastName);
+
+    // select * from author where first_name is like '%al'
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String lastName);
+
+    // select * from author where first_name in ('ali', 'bou', 'coding')
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> lastNames);
 
 }
