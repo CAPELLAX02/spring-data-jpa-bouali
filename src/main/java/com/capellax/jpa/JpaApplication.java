0729dc9312dec64be2ba3e601eb322591da7d9e9
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-11 13:40:38 +0300

    Inserting some data using Faker

--- src/main/java/com/capellax/jpa/JpaApplication.java
+++ src/main/java/com/capellax/jpa/JpaApplication.java
@@ -4,6 +4,7 @@ import com.capellax.jpa.models.Author;
 import com.capellax.jpa.models.Video;
 import com.capellax.jpa.repositories.AuthorRepository;
 import com.capellax.jpa.repositories.VideoRepository;
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,21 +23,26 @@ public class JpaApplication {
 			VideoRepository videoRepository
 	) {
 		return args -> {
-			var author = Author.builder()
-					.firstName("John")
-					.lastName("Doe")
-					.age(25)
-					.email("john@email.com")
-					.build();
+			for (int i = 0; i < 50; i++) {
+				Faker faker = new Faker();
 
-			authorRepository.save(author);
+				var author = Author.builder()
+						.firstName(faker.name().firstName())
+						.lastName(faker.name().lastName())
+						.age(faker.number().numberBetween(18, 70))
+						.email(faker.internet().emailAddress())
+						.build();
 
-			var video = Video.builder()
+				authorRepository.save(author);
+			}
+
+			/*var video = Video.builder()
 					.name("abc")
 					.length(5)
 					.build();
 
 			videoRepository.save(video);
+			 */
 		};
 	}
 
