64715e6b1126e951ec53c772bd75bac480f05295
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-11 12:13:54 +0300

    pretending polymorphic queries using @Polymorphism annotation, yet it is deprecated.

--- src/main/java/com/capellax/jpa/models/Video.java
+++ src/main/java/com/capellax/jpa/models/Video.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Polymorphism;
+import org.hibernate.annotations.PolymorphismType;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -14,7 +16,8 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 @Entity
-@PrimaryKeyJoinColumn(name = "video_id")
+//@PrimaryKeyJoinColumn(name = "video_id")
+@Polymorphism(type = PolymorphismType.EXPLICIT) // default is .IMPLICIT
 public class Video extends Resource {
 
     private int length;
